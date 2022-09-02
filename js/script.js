40355cdb6b32d445542a38168a9137e735dca42a
eleanorcode <innocence17@gmail.com>
2022-09-02 23:02:53 +0300

    Restore basic structure

--- js/script.js
+++ js/script.js
@@ -1,14 +1,1 @@
-const button = document.querySelector('button')
-const body = document.querySelector('body')
 
-button.onclick = () => {
-  if (body.style.backgroundColor === 'rgb(0, 0, 0)') {
-    body.style.backgroundColor = '#c3c3c3'
-    body.style.color = '#000'
-    button.innerText = 'Dark mode'
-  } else {
-    body.style.backgroundColor = '#000'
-    body.style.color = '#fff'
-    button.innerText = 'Light mode'
-  }
-}
