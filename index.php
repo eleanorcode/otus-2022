a03f98266e222991bd09e8f155c091ba7252e945
eleanorcode <innocence17@gmail.com>
2022-10-07 11:55:47 +0300

    Convert HTML to PHP

---
+++ index.php
@@ -0,0 +1,92 @@
+<?php ?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+  <link rel="stylesheet" href="css/normalize.css" />
+  <link rel="stylesheet" href="css/style.css" />
+</head>
+
+<body>
+  <section class="services">
+    <div class="container">
+      <h2>Services that connect you to your users</h2>
+      <div class="services-flex">
+
+        <div class="services-flex__item">
+          <img src="img/01.svg" alt="Icon">
+          <h5>Mobile Application</h5>
+          <p>Lorem ipsum dolor amet, consectetur adipiscing elit. Mattis et sed nam sem tellus erat.</p>
+          <div class='link-flex'>
+            <a href="#">Explore</a>
+            <img src="img/arrow-violet.svg" alt="Arrow">
+          </div>
+        </div>
+
+        <div class="services-flex__item">
+          <img src="img/02.svg" alt="Icon">
+          <h5>Web Application</h5>
+          <p>Lorem ipsum dolor amet, consectetur adipiscing elit. Mattis et sed nam sem tellus erat.</p>
+          <div class='link-flex'>
+            <a href="#">Explore</a>
+            <img src="img/arrow-violet.svg" alt="Arrow">
+          </div>
+        </div>
+
+        <div class="services-flex__item">
+          <img src="img/03.svg" alt="Icon">
+          <h5>User Interface Design</h5>
+          <p>Lorem ipsum dolor amet, consectetur adipiscing elit. Mattis et sed nam sem tellus erat.</p>
+          <div class='link-flex'>
+            <a href="#">Explore</a>
+            <img src="img/arrow-violet.svg" alt="Arrow">
+          </div>
+        </div>
+
+        <div class="services-flex__item">
+          <img src="img/04.svg" alt="Icon">
+          <h5>Strategy and Branding</h5>
+          <p>Lorem ipsum dolor amet, consectetur adipiscing elit. Mattis et sed nam sem tellus erat.</p>
+          <div class='link-flex'>
+            <a href="#">Explore</a>
+            <img src="img/arrow-violet.svg" alt="Arrow">
+          </div>
+        </div>
+
+        <div class="services-flex__item">
+          <img src="img/05.svg" alt="Icon">
+          <h5>Performance Marketing</h5>
+          <p>Lorem ipsum dolor amet, consectetur adipiscing elit. Mattis et sed nam sem tellus erat.</p>
+          <div class='link-flex'>
+            <a href="#">Explore</a>
+            <img src="img/arrow-violet.svg" alt="Arrow">
+          </div>
+        </div>
+
+        <div class="services-flex__item">
+          <img src="img/06.svg" alt="Icon">
+          <h5>Project Management</h5>
+          <p>Lorem ipsum dolor amet, consectetur adipiscing elit. Mattis et sed nam sem tellus erat.</p>
+          <div class='link-flex'>
+            <a href="#">Explore</a>
+            <img src="img/arrow-violet.svg" alt="Arrow">
+          </div>
+        </div>
+      </div>
+      <!--button -->
+      <div class="button-flex">
+        <button>
+          Explore all
+          <img src="img/arrow-white.svg" alt="Arrow">
+        </button>
+      </div>
+    </div>
+  </section>
+
+</body>
+
+</html>
