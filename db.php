edaf0a0038dec4c2012566a50a91d7904d419c67
eleanorcode <innocence17@gmail.com>
2022-11-02 17:56:35 +0300

    Update SQL queries

--- db.php
+++ db.php
@@ -19,7 +19,12 @@ function connect()
 function getBooks(): array
 {
     $pdo = connect();
-    $result = $pdo->query('select * from books order by id desc');
+    $result = $pdo->query(
+        "SELECT book.id, book.title, author.name, book.pages, book.year
+FROM author
+INNER JOIN authors_books ON authors_books.author_id = author.id
+INNER JOIN book ON authors_books.book_id = book.id"
+    );
     $result->execute();
     $data = $result->fetchAll();
     return $data;
@@ -29,7 +34,11 @@ function searchBooks($query): array
 {
     $pdo = connect();
 
-    $sqlQuery = "select * from books where (authors regexp ? or title regexp ?)";
+    $sqlQuery = $sqlQuery = "SELECT book.id, book.title, author.name, book.pages, book.year
+FROM author
+INNER JOIN authors_books ON authors_books.author_id = author.id
+INNER JOIN book ON authors_books.book_id = book.id
+WHERE (author.name like ? or book.title like ?)";
 
     $result = $pdo->prepare($sqlQuery);
     $result->execute([
