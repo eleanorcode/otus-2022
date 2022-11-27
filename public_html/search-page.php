d435a656026d105060144a64973681e3e586804e
eleanorcode <innocence17@gmail.com>
2022-11-28 00:53:03 +0300

    Update search-page.php

--- public_html/search-page.php
+++ public_html/search-page.php
@@ -9,7 +9,7 @@ require_once '../search.php';?>
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=5">
-  <title>Document</title>
+  <title>Library</title>
   <link rel="stylesheet" href="css/normalize.css">
   <link rel="stylesheet" href="css/bootstrap.css">
   <link rel="stylesheet" href="css/style.css">
